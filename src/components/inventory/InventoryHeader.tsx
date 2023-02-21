d9161a1ec7c167bab7407b3b9976d5c7a9dbcd23
taejinii <xowls177@gmail.com>
2023-02-22 02:19:50 +0900

    feat:리덕스 상태에 따른 모달 활성화 구현

--- src/components/inventory/InventoryHeader.tsx
+++ src/components/inventory/InventoryHeader.tsx
@@ -1,26 +1,14 @@
 import axios from "axios";
+import { useAppDispatch } from "../../store/store";
+import { openModal } from "../../store/modalSlice";
 export default function InventoryHeader() {
-  const addItem = () => {
-    axios.post("http://localhost:3001/items", {
-      buyDate: "21-02-26",
-      buyPlace: "World",
-      productName: "Hello",
-      quantity: 5,
-      price: 80,
-      krwPrice: "522,676",
-      shipExpense: "40,000",
-      customsDuty: "World",
-      totalPrice: "9999999999",
-      sellPrice: 98132464,
-      netProfit: "World",
-    });
-  };
+  const dispatch = useAppDispatch();
   return (
     <header>
       <div className="flex justify-between items-center w-full mb-2">
         <h1 className="text-3xl font-semibold">Inventory</h1>
         <div className="flex gap-3">
-          <button onClick={addItem}>아이템 추가</button>
+          <button onClick={() => dispatch(openModal())}>아이템 추가</button>
           <button>엑셀 다운로드</button>
           <button>엑셀 업로드</button>
         </div>
--- src/components/modals/AddItemModal.tsx
+++ src/components/modals/AddItemModal.tsx
@@ -1,16 +1,31 @@
 import styled from "styled-components";
-const ModalContainer = styled.div`
+import { closeModal } from "../../store/modalSlice";
+import { useAppSelector, useAppDispatch } from "../../store/store";
+
+interface VisibleType {
+  visible: boolean;
+}
+
+const ModalContainer = styled.div<VisibleType>`
   position: fixed;
-  display: flex;
+  display: ${(props) => (props.visible ? "block" : "none")};
   flex-direction: column;
   width: 350px;
   height: 350px;
   background-color: blue;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 `;
 export default function AddItemModal() {
+  const dispatch = useAppDispatch();
+  const { isOpen } = useAppSelector((state) => state.modal);
+  console.log(isOpen);
   return (
-    <ModalContainer>
-      <div>d</div>
+    <ModalContainer visible={isOpen}>
+      <button onClick={() => dispatch(closeModal())}>x</button>
     </ModalContainer>
   );
 }
