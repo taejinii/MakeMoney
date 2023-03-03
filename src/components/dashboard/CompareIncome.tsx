ab1b4933a1143fc22baaed1061c53b37c769ff93
taejinii <xowls177@gmail.com>
2023-03-04 03:36:00 +0900

    feat:대쉬보드페이지 차트 구현

--- src/components/dashboard/CompareIncome.tsx
+++ src/components/dashboard/CompareIncome.tsx
@@ -1,36 +1,22 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-
-export default function CompareIncome() {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    const { data } = await axios.get(
-      `http://localhost:3001/items?buyDate_gte=2023-02-01&buyDate_lte=2023-02-31`
-    );
-    setData(data);
-  };
-  console.log("data", data);
-  useEffect(() => {
-    getData();
-  }, []);
+export default function CompareIncome({ data, month, prevMonth }: any) {
   const totalSales = data
     .map((el: any) => Number(el.sellPrice))
-    .reduce((prev, curr) => prev + curr, 0);
+    .reduce((prev: any, curr: any) => prev + curr, 0);
   const totalPurchase = data
     .map((el: any) => Number(el.price) * 1317)
-    .reduce((prev, curr) => prev + curr, 0);
-
+    .reduce((prev: any, curr: any) => prev + curr, 0);
   return (
     <div className="flex flex-col border-2 items-stretch p-4 rounded-2xl">
-      <header className="">
+      <header>
         <h1 className="text-2xl font-bold p-2">Preview</h1>
       </header>
       <main className="flex flex-col ">
-        <div className="flex">
-          <div className="p-2 border-r-2">
+        <div className="flex justify-around">
+          <div className="p-2">
             <h3>Spending</h3>
             {totalPurchase.toLocaleString()}
           </div>
+          <div className="border-r-2"></div>
           <div className="p-2">
             <h3>Sales</h3>
             {totalSales.toLocaleString()}
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,51 @@
 import { Container } from "./InventoryPage";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import CompareIncome from "../components/dashboard/CompareIncome";
 import SalesChart from "../components/dashboard/SalesChart";
+interface selectTypes {
+  id: number;
+  month: string;
+}
 export default function DashboardPage() {
+  const [data, setData] = useState([]);
+  const [month, setMonth] = useState("01");
+  const getData = async () => {
+    const { data } = await axios.get(
+      `http://localhost:3001/items?buyDate_gte=2023-${month}-01&buyDate_lte=2023-${month}-31`
+    );
+    setData(data);
+  };
+  useEffect(() => {
+    getData();
+  }, [month]);
+  const selectList: selectTypes[] = [
+    { id: 1, month: "01" },
+    { id: 2, month: "02" },
+    { id: 3, month: "03" },
+    { id: 4, month: "04" },
+    { id: 5, month: "05" },
+    { id: 6, month: "06" },
+    { id: 7, month: "07" },
+    { id: 8, month: "08" },
+    { id: 9, month: "09" },
+    { id: 10, month: "10" },
+    { id: 11, month: "11" },
+    { id: 12, month: "12" },
+  ];
+  const onMonth = (e: any) => {
+    setMonth(e.target.value);
+  };
   return (
     <Container>
+      <select onChange={onMonth}>
+        {selectList.map((el) => {
+          return <option key={el.id}>{el.month}</option>;
+        })}
+      </select>
       <div className="flex flex-row justify-between">
-        <CompareIncome />
-        <SalesChart />
+        <CompareIncome data={data} month={month} />
+        <SalesChart data={data} month={month} />
       </div>
     </Container>
   );
