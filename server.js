4131ce16da38e0ed0601f9f1de743e18c49d8e15
taejinii <xowls177@gmail.com>
2023-03-07 20:46:32 +0900

    bug:server.js 수정

--- server.js
+++ server.js
@@ -5,13 +5,9 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
+
 server.use(router);
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log("JSON Server is running");
 });
 
