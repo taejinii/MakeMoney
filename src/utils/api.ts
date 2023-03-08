108087f54d2e2a0b55906b0fbc26600d2b905640
taejinii <xowls177@gmail.com>
2023-03-09 02:23:58 +0900

    feat:db.json 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 export const getItem = async () => {
   try {
-    const response = await axios.get(
-      "https://restul-api-json-server-vercel.vercel.app/items"
-    );
+    const response = await axios.get("http://localhost:3001/items");
     return response.data;
   } catch (err) {
     console.log(err);
